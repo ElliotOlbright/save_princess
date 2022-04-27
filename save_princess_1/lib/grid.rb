cf65d6c62f0c3554758e6848a2ff06467a572a17
Elliot Olbright <elliotolbright@gmail.com>
2022-04-27 16:56:07 -0600

    create #locate_princess, add @princess attr_reader

--- save_princess_1/lib/grid.rb
+++ save_princess_1/lib/grid.rb
@@ -1,10 +1,27 @@
 class Grid 
   attr_reader :grid,
-              :grid_size
+              :grid_size,
+              :bot_row,
+              :bot_column,
+              :princess
 
   def initialize(grid_size, grid)
 
     @grid = grid
     @grid_size = grid_size
+    @bot_row = (grid_size - 1) / 2
+    @bot_column = (grid_size - 1) / 2
+    @princess = locate_princess
   end 
+
+  def locate_princess
+    c = []
+    @grid.any? do |s|
+      if s.include?("p")
+        c << @grid.find_index(s)
+        c << s.index('p')
+      end
+    end
+    c
+  end
 end 
