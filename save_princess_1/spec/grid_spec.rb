8000bf3c920f3d7d9ac3c4b1c6fe7684c4f0aeac
Elliot Olbright <elliotolbright@gmail.com>
2022-04-27 16:55:09 -0600

    create 'find princess' test

--- save_princess_1/spec/grid_spec.rb
+++ save_princess_1/spec/grid_spec.rb
@@ -5,15 +5,25 @@ describe Grid do
   it 'initiates with valid grid size' do
     grid_a = Grid.new(3, ["m--", "-p-", "---"])
 
+    expect(grid_a).to be_a Grid
     expect(grid_a.grid.size).to eq(3)
+    expect(grid_a.grid).to eq(["m--", "-p-", "---"])
   end
 
   it 'can get correct bot coordinates' do
-    grid_b = Grid.new(3, ["m--", "-p-", "---"])
+    grid_b = Grid.new(4, ["m--", "-p-", "---", "---"])
 
     expect(grid_b.bot_row).to eq(1)
     expect(grid_b.bot_column).to eq(1)
   end
 
+  it 'can find princess' do
+    grid_a = Grid.new(3, ["m--", "-p-", "---"])
+    grid_b = Grid.new(3, ["--p", "---", "--m"])
+
+
+    expect(grid_a.princess).to eq([1, 1])
+    expect(grid_b.princess).to eq([0, 2])
+  end
 end 
 
