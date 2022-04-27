11b941152584d2adc82aad0ec4b8e5e8817a6ba1
Elliot Olbright <elliotolbright@gmail.com>
2022-04-27 16:18:10 -0600

    begin grid testing

---
+++ save_princess_1/spec/grid_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+require '../lib/grid/'
+
+describe Grid do
+  it 'should instantiate with correct grid size' do
+    grid_1 = Grid.new(3, ["---", "-m-", "p--"])
+
+    expect(grid_1.grid.size).to eq(3)
+  end
+end 
