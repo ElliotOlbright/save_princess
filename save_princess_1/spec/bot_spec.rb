fbc38847087d00c9f03de41fcd2ec808e50234a4
Elliot Olbright <elliotolbright@gmail.com>
2022-04-27 17:53:38 -0600

    delete uneccessary repeats

--- save_princess_1/spec/bot_spec.rb
+++ save_princess_1/spec/bot_spec.rb
@@ -1,8 +1,6 @@
 require 'spec_helper'
 require_relative '../lib/bot/'
 
-
-
 describe Bot do
   it 'is an instance of Bot' do
     grid = Grid.new(3, ["---", "-m-", "p--"])
@@ -12,13 +10,9 @@ describe Bot do
   end
 
   it 'can show coordinates for bot' do
-    grid = Grid.new(3, ["--m", "---", "--p"])
-    bot = Bot.new(grid)
     grid_2 = Grid.new(3, ["---", "-m-", "--p"])
     bot_2 = Bot.new(grid_2)
 
-    expect(bot.bot_x).to eq(0)
-    expect(bot.bot_y).to eq(2)
     expect(bot_2.bot_x).to eq(1)
     expect(bot_2.bot_y).to eq(1)
   end
