81d378d38f51ebd466905abc336e27c7322164da
Elliot Olbright <elliotolbright@gmail.com>
2022-04-28 15:14:07 -0600

    update hackerrank runner files

--- bsp_2_HR_runner.rb
+++ bsp_2_HR_runner.rb
@@ -1,88 +1,62 @@
-class Grid
-  attr_reader :grid_size, 
-              :grid,
-              :princess_x,
-              :princess_y
+cclass Grid
+attr_reader :grid_size, 
+            :grid,
+            :princess_x,
+            :princess_y
 
-  def initialize(grid)
-    @grid_size = grid.count
-    @grid = grid
-    @princess_x = locate_princess[0]
-    @princess_y = locate_princess[1]
-  end 
+def initialize(grid)
+  @grid_size = grid.count
+  @grid = grid
+  @princess_x = locate_princess[0]
+  @princess_y = locate_princess[1]
+end 
 
-  def locate_princess
-    princess_coords = []
-    @grid.any? do |s|
-      if s.include?("p")
-        princess_coords << s.index('p')
-        princess_coords << @grid.find_index(s)
-      end
+def locate_princess
+  princess_coords = []
+  @grid.any? do |s|
+    if s.include?("p")
+      princess_coords << s.index('p')
+      princess_coords << @grid.find_index(s)
     end
-    princess_coords
   end
+  princess_coords
+end
 end 
 
 class Findprincess
-  attr_reader :grid,
-                :bot_x,
-                :bot_y,
-                :princess_x,
-                :princess_y,
-                :step
+attr_reader :grid,
+              :bot_x,
+              :bot_y,
+              :princess_x,
+              :princess_y,
+              :step
 
-  def initialize(n, row, column, grid)
-    @grid = Grid.new(grid)
-    @bot_x = column
-    @bot_y = row
-    @princess_x = @grid.princess_x
-    @princess_y = @grid.princess_y
-    @step = []
-  end
+def initialize(n, row, column, grid)
+  @grid = Grid.new(grid)
+  @bot_x = column
+  @bot_y = row
+  @princess_x = @grid.princess_x
+  @princess_y = @grid.princess_y
+end
 
-  def next_step
-    if (@bot_y == @princess_y)
-      horizontal_movement
-    else 
-      vertical_movement
-    end
-    puts @step
-  end
+def next_step
+  puts generate_row_move || generate_column_move
+end
 
-  def horizontal_movement
-    if @bot_x > @princess_x
-      @bot_x -= 1
-      @step << 'LEFT'
-    elsif @bot_x < @princess_x
-      @bot_x += 1
-      @step << 'RIGHT'
-    end
-  end
+def generate_column_move
+  column_diff = @princess_x - @bot_x
+  (column_diff.negative? ? 'LEFT' : 'RIGHT') if !column_diff.zero?
+end
 
-  def vertical_movement
-    if @bot_x == @princess_x
-      if @bot_y > @princess_y
-        @bot_y -= 1
-        @step << 'UP'
-      elsif @bot_y < @princess_y
-        @bot_y += 1
-        @step << 'DOWN'
-      end
-    else
-      if @bot_y > @princess_y
-        @bot_y -= 1
-        @step << 'UP'
-      elsif @bot_y < @princess_y
-        @bot_y += 1
-        @step << 'DOWN'
-      end
-    end
-  end
+def generate_row_move
+  row_diff = @princess_y - @bot_y
+  (row_diff.negative? ? 'UP' : 'DOWN') if !row_diff.zero?
+end 
 end
 
 def nextMove(n,r,c,grid)
-  save_princess = Findprincess.new(n, r, c, grid)
-  save_princess.next_step
+save_princess = Findprincess.new(n, r, c, grid)
+save_princess.next_step
 end
 
 n = gets.to_i
@@ -92,7 +66,7 @@ r,c = gets.strip.split.map {|num| num.to_i}
 grid = Array.new(n)
 
 (0...n).each do |i|
-  grid[i] = gets
+grid[i] = gets
 end
 
 nextMove(n,r,c,grid)
