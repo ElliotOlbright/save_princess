6995265512d96bdee3a3ded0c42a2eb12ff4fb8b
Elliot Olbright <elliotolbright@gmail.com>
2022-04-27 23:06:02 -0600

    create find princess method and test

--- save_princess_2/lib/grid.rb
+++ save_princess_2/lib/grid.rb
@@ -1,5 +1,21 @@
 class Grid
+  attr_reader :grid_size, 
+              :grid
+
   def initialize(grid_size, grid)
-    
+
+    @grid_size = grid_size
+    @grid = grid
   end 
+
+  def find_princess
+    princess_coords = []
+    @grid.any? do |s|
+      if s.include?("p")
+        princess_coords << @grid.find_index(s)
+        princess_coords << s.index('p')
+      end
+    end
+    princess_coords
+  end
 end 
--- save_princess_2/spec/grid_spec.rb
+++ save_princess_2/spec/grid_spec.rb
@@ -2,9 +2,22 @@ require 'spec_helper'
 require_relative '../lib/grid/'
 
 describe Grid do
-    it 'can create a grid' do
-      grid = Grid.new(5, ["-----", "-----", "p-m--", "-----", "-----"])
+  before :each do 
+    @grid = Grid.new(5, ["-----", "-----", "p-m--", "-----", "-----"])
+  end 
 
-      expect(grid).to be_a Grid
-    end
+  it 'can create a grid' do
+
+    expect(@grid).to be_a Grid
   end
+
+  it 'has attributes' do
+    expect(@grid.grid_size).to eq(5)
+    expect(@grid.grid).to be_an Array
+    expect(@grid.grid).to eq(["-----", "-----", "p-m--", "-----", "-----"])
+  end 
+
+  it 'can find the princess' do 
+    expect(@grid.find_princess).to eq([2,0])
+  end 
+end 
