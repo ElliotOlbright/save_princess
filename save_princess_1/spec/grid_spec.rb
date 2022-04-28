f9480057cd9228e3703562c9f640045df97d8755
Elliot Olbright <elliotolbright@gmail.com>
2022-04-27 18:14:50 -0600

    add before :each to testing

--- save_princess_1/spec/grid_spec.rb
+++ save_princess_1/spec/grid_spec.rb
@@ -2,40 +2,37 @@ require 'spec_helper'
 require_relative '../lib/grid/'
 
 describe Grid do
+  before :each do
+    @grid = Grid.new(3, ["m--", "-p-", "---"])
+    @bot_row = @grid.bot[0]
+    @bot_column = @grid.bot[1]
+  end 
   it 'initiates with valid grid size' do
-    grid_a = Grid.new(3, ["m--", "-p-", "---"])
-
-    expect(grid_a).to be_a Grid
-    expect(grid_a.grid.size).to eq(3)
-    expect(grid_a.grid).to eq(["m--", "-p-", "---"])
+    expect(@grid).to be_a Grid
+    expect(@grid.grid.size).to eq(3)
+    expect(@grid.grid).to eq(["m--", "-p-", "---"])
   end
 
   it 'can get correct bot coordinates' do
-    grid_a = Grid.new(4, ["----", "-p--", "----", "m---"])
-
-    bot_row = grid_a.bot[0]
-    bot_column = grid_a.bot[1]
-
-    expect(bot_row).to eq(3)
-    expect(bot_column).to eq(0)
+    expect(@bot_row).to eq(0)
+    expect(@bot_column).to eq(0)
   end
 
   it 'can find princess' do
-    grid_a = Grid.new(3, ["m--", "-p-", "---"])
-    grid_b = Grid.new(3, ["--p", "---", "--m"])
-
-
-    expect(grid_a.princess).to eq([1, 1])
-    expect(grid_b.princess).to eq([0, 2])
+    expect(@grid.princess).to eq([1, 1])
   end
 end 
 
 describe 'Grid Sadpath' do
   it 'returns an error if grid_size above 100' do
     expect {Grid.new(500, ["m--", "-p-", "---"])}.to raise_error(ArgumentError)
-    expect {Grid.new(2, ["m--", "-p-", "---"])}.to raise_error(ArgumentError)
     expect {Grid.new(3, ["m--", "-p-", "---"])}.not_to raise_error
   end
+
+  it 'returns an error if grid_size below 3' do
+    expect {Grid.new(2, ["m--", "-p-", "---"])}.to raise_error(ArgumentError)
+    expect {Grid.new(3, ["m--", "-p-", "---"])}.not_to raise_error
+  end 
 
   it 'returns error if grid and grid size to do not match' do
     expect {Grid.new(3, ["m---", "-p--", "----", "----"])}.to raise_error(ArgumentError)
