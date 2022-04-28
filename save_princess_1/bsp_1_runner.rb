6e02146d795bf8679014a9bffc8f97234bde4025
Elliot Olbright <elliotolbright@gmail.com>
2022-04-27 22:01:17 -0600

    fix instructions in runner prompt

--- save_princess_1/bsp_1_runner.rb
+++ save_princess_1/bsp_1_runner.rb
@@ -6,7 +6,7 @@ grid_size = gets.chomp.to_i
 row = 0
 
 until row == grid_size
-  puts "please provide a row for you grid #{row}, keep in mind 'm' has to be placed in the center of the grid. Ex. '--p', '-m-', '---'"
+  puts "please provide a row for you grid (#{row}), keep in mind 'm' has to be placed in the center of the grid. Ex. '--p', '-m-', '---'"
   grid << gets.chomp
   row += 1
 end 
