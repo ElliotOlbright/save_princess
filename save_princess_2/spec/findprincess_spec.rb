40fa4a9f9a29ad9704809476d870067b20e42a1f
Elliot Olbright <elliotolbright@gmail.com>
2022-04-28 07:58:52 -0600

    create attributes test

--- save_princess_2/spec/findprincess_spec.rb
+++ save_princess_2/spec/findprincess_spec.rb
@@ -4,9 +4,21 @@ require_relative '../lib/grid'
 
 
 describe Findprincess do
-  it 'can create an instance of Findprincess' do
-    path = Findprincess.new(5, 2, 3, ["-----\n", "-----\n", "p--m-\n", "-----\n", "-----\n"])
-
-    expect(path.grid).to be_a Grid
+  before :each do
+    @path = Findprincess.new(5, 2, 3, ["-----\n", "-----\n", "p--m-\n", "-----\n", "-----\n"])
   end
+
+  it 'can create an instance of Findprincess' do
+
+    expect(@path.grid).to be_a Grid
+  end
+
+  it 'has attributes' do
+    expect(@path.grid.grid_size).to eq(5)
+    expect(@path.bot_x).to eq(2)
+    expect(@path.bot_y).to eq(3)
+    expect(@path.princess_x).to eq(0)
+    expect(@path.princess_y).to eq(2)
+
+  end 
 end 
